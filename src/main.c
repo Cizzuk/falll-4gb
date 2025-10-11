20f7393409b7d9cb01efac6805c1626ce96c1e6e
Cizzuk <dropcat@cizzuk.net>
2025-10-11 18:18:38 +0900

    Add leaves

--- src/main.c
+++ src/main.c
@@ -1,12 +1,54 @@
 #include <gb/gb.h>
 #include "sprites.h"
 
+// Screen constants
+// #define SCREEN_WIDTH 160
+// #define SCREEN_HEIGHT 144
+// #define SCREEN_TOP 16
+// #define SCREEN_BOTTOM (SCREEN_TOP + SCREEN_HEIGHT)
+// #define SCREEN_LEFT 8
+// #define SCREEN_RIGHT (SCREEN_LEFT + SCREEN_WIDTH)
+
 BOOLEAN is_gaming = TRUE;
 BOOLEAN player_flip = FALSE;
 UINT8 leaf_frame = 0;
 
 // screen has dead zones, left: 8px, top: 16px
 UINT8 player_pos[2] = {63, 32}; // X: 36 - 90
+UINT8 leaves_pos[3][2] = {{43, 160}, {63, 0}, {83, 0}};
+
+void init_sprites(void) {
+    // Player
+    set_sprite_tile(0, 0);
+    set_sprite_tile(1, 2);
+    set_sprite_tile(2, 1);
+    set_sprite_tile(3, 3);
+
+    // Leaves
+    // Leaf 1
+    set_sprite_tile(4, 6);
+    set_sprite_tile(5, 4);
+    set_sprite_tile(6, 7);
+    set_sprite_tile(7, 5);
+    set_sprite_prop(4, S_FLIPX);
+    set_sprite_prop(5, S_FLIPX);
+    set_sprite_prop(6, S_FLIPX);
+    set_sprite_prop(7, S_FLIPX);
+    // Leaf 2
+    set_sprite_tile(8, 5);
+    set_sprite_tile(9, 7);
+    set_sprite_tile(10, 4);
+    set_sprite_tile(11, 6);
+    set_sprite_prop(8, S_FLIPY);
+    set_sprite_prop(9, S_FLIPY);
+    set_sprite_prop(10, S_FLIPY);
+    set_sprite_prop(11, S_FLIPY);
+    // Leaf 3
+    set_sprite_tile(12, 4);
+    set_sprite_tile(13, 6);
+    set_sprite_tile(14, 5);
+    set_sprite_tile(15, 7);
+}
 
 void render_player(void) {
     if (player_flip) {
@@ -50,6 +92,51 @@ void player_control(void) {
     }
 }
 
+void render_leaves(void) {
+    // Leaf 1
+    if (leaves_pos[0][1] != 0) {
+        move_sprite(4, leaves_pos[0][0], leaves_pos[0][1]);
+        move_sprite(5, leaves_pos[0][0] + 8, leaves_pos[0][1]);
+        move_sprite(6, leaves_pos[0][0], leaves_pos[0][1] + 8);
+        move_sprite(7, leaves_pos[0][0] + 8, leaves_pos[0][1] + 8);
+    }
+    // Leaf 2
+    if (leaves_pos[1][1] != 0) {
+        move_sprite(8, leaves_pos[1][0], leaves_pos[1][1]);
+        move_sprite(9, leaves_pos[1][0] + 8, leaves_pos[1][1]);
+        move_sprite(10, leaves_pos[1][0], leaves_pos[1][1] + 8);
+        move_sprite(11, leaves_pos[1][0] + 8, leaves_pos[1][1] + 8);
+    }
+    // Leaf 3
+    if (leaves_pos[2][1] != 0) {
+        move_sprite(12, leaves_pos[2][0], leaves_pos[2][1]);
+        move_sprite(13, leaves_pos[2][0] + 8, leaves_pos[2][1]);
+        move_sprite(14, leaves_pos[2][0], leaves_pos[2][1] + 8);
+        move_sprite(15, leaves_pos[2][0] + 8, leaves_pos[2][1] + 8);
+    }
+}
+
+void leaves_scroll(void) {
+    if (leaves_pos[0][1] > 0) {
+        leaves_pos[0][1]--;
+    }
+    if (leaves_pos[1][1] > 0) {
+        leaves_pos[1][1]--;
+    }
+    if (leaves_pos[2][1] > 0) {
+        leaves_pos[2][1]--;
+    }
+    if (leaf_frame % 60 == 0) {
+        if (leaves_pos[0][1] == 0) {
+            leaves_pos[0][1] = 160;
+        } else if (leaves_pos[1][1] == 0) {
+            leaves_pos[1][1] = 160;
+        } else if (leaves_pos[2][1] == 0) {
+            leaves_pos[2][1] = 160;
+        }
+    }
+}
+
 void reset_game(void) {
     is_gaming = TRUE;
     player_flip = FALSE;
@@ -60,12 +147,7 @@ void reset_game(void) {
 
 void main(void) {
     set_sprite_data(0, 15, Sprites);
-
-    // Player
-    set_sprite_tile(0, 0);
-    set_sprite_tile(1, 2);
-    set_sprite_tile(2, 1);
-    set_sprite_tile(3, 3);
+    init_sprites();
 
     DISPLAY_ON;
     SHOW_SPRITES;
@@ -82,7 +164,9 @@ void main(void) {
             }
         }
 
+        leaves_scroll();
         render_player();
+        render_leaves();
         wait_vbl_done();
     }
 }
