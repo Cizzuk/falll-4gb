5c1332f89feb37a238b2a8912d5a0d08d1aea352
Cizzuk <dropcat@cizzuk.net>
2025-11-03 16:31:51 +0900

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -30,7 +30,7 @@ inline UINT8 uint8_random(UINT8 min, UINT8 max) {
 }
 
 inline BOOLEAN check_collision(UINT8 a_left, UINT8 a_top, UINT8 a_right, UINT8 a_bottom,
-                        UINT8 b_left, UINT8 b_top, UINT8 b_right, UINT8 b_bottom) {
+                               UINT8 b_left, UINT8 b_top, UINT8 b_right, UINT8 b_bottom) {
     return (a_top <= b_bottom) && (a_bottom >= b_top) && (a_left <= b_right) && (a_right >= b_left);
 }
 
